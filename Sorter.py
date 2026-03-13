431c5c7a1da93f8c01eda259d19d780374d515bb
ecngujo <ecngujo@gmail.com>
2026-03-13 15:18:05 +0800

    softcoded download folder location

--- Sorter.py
+++ Sorter.py
@@ -3,8 +3,19 @@
 import os
 import shutil
 import getpass
+import platform
+import sys
 
 os_username = getpass.getuser()
-os_name = platform.system()
+os_name = platform.system() ## For future use?
+
+# Default properties only! May not work with modified downloads location\
+downloads_folder = f"C:\\Users\\{os_username}\\Downloads"
+
+if not os.path.isdir(downloads_folder):
+  print("Download folder not found!")
+  sys.exit()
+
+categories = ["mp3", "mp4", "pdf", "docx", "png", "jpg"]
 
 

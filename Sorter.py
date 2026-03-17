5ca8fdd596c4855bfc1b5e1fd432d80429468c17
ecngujo <ecngujo@gmail.com>
2026-03-17 16:47:59 +0800

    Completely changed the sorting algorithm and ommited some unnecesary stuff

--- Sorter.py
+++ Sorter.py
@@ -1,19 +1,15 @@
 # Random project! 3/11/2026
 
-import os
 import shutil
 import getpass
-import platform
 import sys
 from pathlib import Path
 
-# Just for prints :D
-debug = True
-
 os_username = getpass.getuser()
-os_name = platform.system() ## For future use?
 
-# Default properties only! May not work with modified downloads location
+excluded = ["c", "ico", "png"]
+
+# Default properties only! May not work with modified downloads location :P
 root = Path("C:/") / "Users" / os_username
 downloads_folder = Path(root) / "Downloads"
 desktop_folder = Path(root) / "Desktop"
@@ -25,32 +21,18 @@ if not downloads_folder.is_dir():
 
 # Storage for the category folders
 if not container_folder.is_dir():
-  print("Created container..")
+  print("Creating container..")
   Path(container_folder).mkdir()
 
-
-categories = ["mp3", "mp4", "pdf", "docx", "png", "jpg", "exe", "zip", "pptx"]
-
-# Check if the categories exists in the container, if not then create
-for category in categories:
-  location = os.path.join(container_folder, category)
-  if not os.path.isdir(location):
-    os.mkdir(location)
-    print(f"Created {category} in {container_folder}!")
-
-# Sorting
-
-filesSorted = 0
-for file in os.listdir(downloads_folder):
-  file_location = os.path.join(downloads_folder, file)
-  if not os.path.isdir(file_location):
-    for category in categories:
-      category_location = os.path.join(container_folder, category)
-      if Path(file).suffix == f".{category}":
-        # shutil.move(file_location, category_location)
-        if debug == True:
-          print(f"Moved {file[0:5]}..{Path(file).suffix} -> {category}!")
-        filesSorted += 1
-# I think this is pretty bad algorithm, it's a nested for loop. Not viable for larger files. I will refactor this
-
-print(f"Total files sorted: {filesSorted}")
+for file in downloads_folder.iterdir():
+  fileSuffix = file.suffix.replace(".", "")
+  if file.is_file() and str(fileSuffix) not in excluded:
+    try:
+      folder = Path(downloads_folder) / fileSuffix
+      folder.mkdir(exist_ok=True)
+      shutil.move(str(file), str(Path(folder) / file.name))
+      print(f"{file.name[0:5]}.. -> {folder}")
+    except FileNotFoundError:
+      print(f"{file.name} not found or doesn't exist!")
+    except Exception as e:
+      print(f"Error found: {e}")
